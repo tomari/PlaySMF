078f5d03fb220a659a760c12a8e557ba6f51a870
tomari <davy14008500@users.sourceforge.net>
2013-11-06 18:29:12 +0900

    Refactor

--- src/su/fb01ctl/PlaySMF.java
+++ src/su/fb01ctl/PlaySMF.java
@@ -2,6 +2,7 @@
 package su.fb01ctl;
 
 import java.io.File;
+import java.io.IOException;
 
 import javax.sound.midi.InvalidMidiDataException;
 import javax.sound.midi.MidiDevice;
@@ -15,7 +16,8 @@ import javax.sound.midi.Transmitter;
 
 public class PlaySMF {
 	private static final String help=
-			"PlaySMF [-p dev] [-r gm|gs|xg|mu100|doc|cm32|fb01] 1.mid 2.mid 3.mid ...";
+			"PlaySMF Version 1\n"+
+			"PlaySMF [-p dev] [-r gm|gs|sc88|xg|mu100|doc|mt32|fb01] [-l|-la] 1.mid 2.mid 3.mid ...";
 	public static final byte[][] RESET_GM={{(byte)0xF0,0x7E,0x7F,0x09,0x01,(byte)0xF7}};
 	public static final byte[][] RESET_GS={
 		{(byte)0xF0,0x7E,0x7F,0x09,0x01,(byte)0xF7}, // GM
@@ -31,88 +33,156 @@ public class PlaySMF {
 	public static final byte[][] RESET_DOC={
 		{(byte)0xF0,0x7E,0x7F,0x09,0x01,(byte)0xF7}, // GM
 		{(byte)0xF0,0x43,0x73,0x01,0x14,(byte)0xf7}}; // Disk Orchestra
-	public static final byte[][] RESET_CM32={
+	public static final byte[][] RESET_MT32={
 		{(byte)0xF0,0x41,0x10,0x16,0x11,0x7f,0x00,0x00,0x01,0x00,(byte)0xf7}}; // MT-32 Reset all
 	public static final byte[][] RESET_FB01={
 		{(byte)0xF0,0x43,0x75,0x00,0x20,0x40,0x11,(byte)0xf7}}; // Store configuration
+	public static final byte[][] RESET_SC88={
+		{(byte)0xF0,0x7E,0x7F,0x09,0x01,(byte)0xF7}, // GM
+		{(byte)0xF0,0x41,0x10,0x42,0x12,0x00,0x00,0x7F,0x00,0x01,(byte)0xF7}}; // System mode set mode-1
+	private MidiDevice outDevice;
+	private int outDeviceNum;
+	private byte[][] resetSequence;
+	private Receiver outPort;
+	private Sequencer sequencer;
 	
-	public static void main(String[] args) {
-		byte[][] resetSequence={};
+	protected PlaySMF(int deviceNum,String resetSeqLabel) {
+		outDeviceNum=deviceNum;
+		if(resetSeqLabel==null) {
+			resetSequence=null;
+		} else {
+			resetSequence=chooseResetSequenceByName(resetSeqLabel);
+		}
+	}
+	public void prepare() throws MidiUnavailableException {
+		if(outDeviceNum>0) {
+			MidiDevice.Info[] devInfo=MidiSystem.getMidiDeviceInfo();
+			outDevice=MidiSystem.getMidiDevice(devInfo[outDeviceNum]);
+		} else {
+			outDevice=MidiSystem.getSequencer();
+		}
+		outDevice.open();
+		// connect sequencer to the synthesizer
+		sequencer=MidiSystem.getSequencer();
+		Transmitter seqTx=sequencer.getTransmitter();
+		outPort=outDevice.getReceiver();
+		seqTx.setReceiver(outPort);
+	}
+	public void play(String path) throws InvalidMidiDataException, IOException, MidiUnavailableException {
+		File midFile=new File(path);
+		Sequence midSeq=MidiSystem.getSequence(midFile);
+		if(resetSequence!=null) {
+			sendReset();
+			try {
+				Thread.sleep(50); // Delay after sending reset
+			} catch (InterruptedException e) { }
+		}
+		sequencer.open();
+		sequencer.setSequence(midSeq);
+		long us=sequencer.getMicrosecondLength();
+		sequencer.start();
+		try {
+			Thread.sleep(us/1000);
+		} catch (InterruptedException e) {	
+		} finally {
+			sequencer.stop();
+			sequencer.close();
+		}
+	}
+	public void close() {
+		outDevice.close();
+	}
+	private static String stringOfMidDevPorts(int x) {
+		if(x==-1) { return "Unlimited"; } else return String.valueOf(x);
+	}
+	public static void dumpMidiDevices(boolean dumpAll) {
 		MidiDevice.Info[] devInfo=MidiSystem.getMidiDeviceInfo();
+		for(int i=0; i<devInfo.length; i++) {
+			MidiDevice.Info nfo=devInfo[i];
+			try {
+				MidiDevice dev = MidiSystem.getMidiDevice(nfo);
+				int maxRx=dev.getMaxReceivers();
+				int maxTx=dev.getMaxTransmitters();
+				if(dumpAll || maxRx!=0) {
+					System.out.println("Dev "+String.valueOf(i)+" "+nfo.getVendor()+" "+nfo.getName()+" "+nfo.getVersion());
+					System.out.println("    "+nfo.getDescription());
+					System.out.println("    Source: "+stringOfMidDevPorts(maxTx)+" Sink: "+stringOfMidDevPorts(maxRx));
+				}
+			} catch (MidiUnavailableException e) {
+				System.out.println("Dev "+String.valueOf(i)+" unavailable");
+				continue;
+			}
+		}
+		return;
+	}
+	public static void main(String[] args) {
 		int argptr=0;
+		int outPortId=-1;
+		String resetType=null;
 		if(args.length==0) {
 			System.err.println(help);
-			for(int i=0; i<devInfo.length; i++) {
-				System.out.println("Dev "+String.valueOf(i)+" "+devInfo[i].toString());
-				System.out.println("    "+devInfo[i].getDescription());
-			}
+			dumpMidiDevices(false);
 			System.exit(1);
-			return;
 		}
-		MidiDevice outPort;
 		try {
-			outPort=MidiSystem.getSynthesizer();
 			while(argptr<args.length) {
 				if(args[argptr].equals("-p")) {
-					int x=Integer.valueOf(args[argptr+1]);
-					outPort=MidiSystem.getMidiDevice(devInfo[x]);
+					outPortId=Integer.valueOf(args[argptr+1]);
 					argptr+=2;
 				} else if(args[argptr].equals("-r")) {
-					final String resetType=args[argptr+1];
-					resetSequence=chooseResetSequenceByName(resetType);
+					resetType=args[argptr+1];
 					argptr+=2;
+				} else if(args[argptr].equals("-l")) {
+					dumpMidiDevices(false);
+					System.exit(0);
+					return;
+				} else if(args[argptr].equals("-la")) {
+					dumpMidiDevices(true);
+					argptr++;
+					System.exit(0);
+					return;
 				} else {
 					break;
 				}
 			}
-			outPort.open();
 		} catch (ArrayIndexOutOfBoundsException e) {
 			System.err.println(help);
 			System.exit(1);
 			return;
-		} catch (MidiUnavailableException e) {
+		} catch (NumberFormatException e) {
 			System.err.println(e.toString());
 			System.exit(1);
 			return;
 		}
-		// connect sequencer to the synthesizer
-		Sequencer seq;
-		Receiver synRx;
+		PlaySMF ps=new PlaySMF(outPortId,resetType);
 		try {
-			seq=MidiSystem.getSequencer();
-			Transmitter seqTx=seq.getTransmitter();
-			synRx=outPort.getReceiver();
-			seqTx.setReceiver(synRx);
+			ps.prepare();
 		} catch (MidiUnavailableException e) {
 			System.err.println(e.toString());
-			System.exit(1);
-			return;
 		}
+		
 		for(int i=argptr; i<args.length; i++) {
-			try{
-				File midFile=new File(args[i]);
-				Sequence midSeq=MidiSystem.getSequence(midFile);
-				sendReset(synRx,resetSequence);
-				seq.open();
-				seq.setSequence(midSeq);
-				long us=seq.getMicrosecondLength();
-				seq.start();
-				Thread.sleep(us/1000);
-				seq.stop();
-				seq.close();
+			try {
+				ps.play(args[i]);
 			} catch (Exception e) {
 				System.err.println(e.toString());
 			}
 		}
-		outPort.close();
+		ps.close();
 	}
-	static private void sendReset(Receiver rcv, byte[][] resetSequence) throws InvalidMidiDataException {
+	private void sendReset() {
 		for(int i=0; i<resetSequence.length; i++) {
-			SysexMessage sm=new SysexMessage(resetSequence[i],resetSequence[i].length);
-			rcv.send(sm,-1);
+			SysexMessage sm;
+			try {
+				sm=new SysexMessage(resetSequence[i],resetSequence[i].length);
+			} catch (InvalidMidiDataException e) {
+				e.printStackTrace();
+				return;
+			}
+			outPort.send(sm,-1);
 		}
 	}
-	static private byte[][] chooseResetSequenceByName(String seqName) {
+	public static byte[][] chooseResetSequenceByName(String seqName) {
 		if(seqName.equalsIgnoreCase("gm")) {
 			return RESET_GM;
 		} else if(seqName.equalsIgnoreCase("gs")) {
@@ -123,13 +193,14 @@ public class PlaySMF {
 			return RESET_MU100;
 		} else if(seqName.equalsIgnoreCase("doc")) {
 			return RESET_DOC;
-		} else if(seqName.equalsIgnoreCase("cm32")) {
-			return RESET_CM32;
+		} else if(seqName.equalsIgnoreCase("mt32")) {
+			return RESET_MT32;
 		} else if(seqName.equalsIgnoreCase("fb01")) {
 			return RESET_FB01;
+		} else if(seqName.equalsIgnoreCase("sc88")) {
+			return RESET_SC88;
 		} else {
-			System.err.println("Unknown reset sequence name specified.");
-			return RESET_GM;
+			throw new RuntimeException("Unknown reset sequence specified");
 		}
 	}
 }
